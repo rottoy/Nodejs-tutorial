e1dc19ca26e4f7a597cf300f63f473ffe0814075
rottoy <wnsgur9609@naver.com>
2020-08-04 01:48:14 +0900

    파일 시스템을 통한 동적 웹페이지 생성

---
+++ node/ch3_filesystem/template_main.js
@@ -0,0 +1,45 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url,true).query;
+    var title=queryData.id;
+    console.log(queryData);
+    if(_url == '/'){
+      _url = '/index.html';
+    }
+    if(_url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
+
+    //200(OK)를 반환
+    response.writeHead(200);
+    fs.readFile(`../data/${queryData.id}`,'utf-8',function(err,description){
+        var template=`
+            <!doctype html>
+        <html>
+        <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+        </head>
+        <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        <p>${description}</p>
+        </body>
+        </html>
+            `;
+        response.end(template);
+    })
+    
+   
+
+});
+app.listen(3000);
