6c2256d26f28224a92086506c6c57cf308127d71
rottoy <wnsgur9609@naver.com>
2020-08-04 23:35:58 +0900

    파일 생성, 수정 링크 생성, 수정 정보 전송 및 저장 완료

--- node/POSTwithForm_main.js
+++ node/POSTwithForm_main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 //HTML template 반환
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
         return `
         <!doctype html>
     <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body){
     <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">create</a>
+    ${control}
     ${body}
     </body>
     </html>
@@ -52,7 +52,9 @@ var app = http.createServer(function(request,response){
                 var title='Welcome'; 
                 var description = 'Hello, Node.js';
                 var list =templateList(filelist);
-                var template=templateHTML(title,list,`<h2>${title}</h2>${description}`);
+                var template=templateHTML(title,list,
+                    `<h2>${title}</h2>${description}`,
+                    `<a href="/create">create</a>`);
 
                 
                 response.writeHead(200);//200(OK)를 반환
@@ -64,7 +66,9 @@ var app = http.createServer(function(request,response){
                 fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
                     var title=queryData.id;
                     var list =templateList(filelist);
-                    var template=templateHTML(title,list,`<h2>${title}</h2>${description}`);
+                    var template=templateHTML(title,list,
+                        `<h2>${title}</h2>${description}`,
+                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
                     //200(OK)를 반환
                     response.writeHead(200);
                     response.end(template);
@@ -91,7 +95,7 @@ var app = http.createServer(function(request,response){
                 <input type="submit">
             </p>
             </form>
-            `);
+            `,``);
             //200(OK)를 반환
             response.writeHead(200);
             response.end(template);
@@ -129,6 +133,60 @@ var app = http.createServer(function(request,response){
         });
 
      
+    }
+    else if(pathname==='/update'){
+        fs.readdir('./data',function(error,filelist){
+            fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
+                var title=queryData.id;
+                var list =templateList(filelist);
+                var template=templateHTML(title,list,
+                    `  <form action="http://localhost:3000/update_process" method='POST'>
+                    <input type="hidden" name="id" value="${title}">
+                    <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                    <p>
+                        <textarea name="description" placeholder="description">${description}</textarea>
+                    </p>
+                    <p>
+                        <input type="submit">
+                    </p>
+                    </form>`,
+                    `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+                //200(OK)를 반환
+                response.writeHead(200);
+                response.end(template);
+            });
+        });
+    }
+    else if(pathname==='/update_process'){
+        var body='';
+
+        //request.on : 데이터가 부분으로 들어옴
+        request.on('data', function(data){
+            body= body+data;
+            //body is POST data
+
+        });
+
+        //request.end : 정보 수신이 끝났을 때 실행
+        request.on('end',function(){
+            
+            //return json of body
+             var post = qs.parse(body);
+             var id =post.id;
+             var title=post.title;
+             var description=post.description;
+             console.log(post);
+             //File rename
+             fs.rename(`data/${id}`,`data/${title}`,function(err){
+                fs.writeFile(`data/${title}`,description,'utf-8',function(err){
+                    //Redirection : 302
+                    response.writeHead(302,{Location : `/?id=${title}`});
+                    response.end('success');
+                 })
+    
+
+             });
+        });
     }
     else{
         response.writeHead(404);
