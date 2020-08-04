c077e1b2d03307d0a319ecb7a77ee30043743cba
JeonHuiJae <rottoy@konkuk.ac.kr>
2020-08-04 16:01:09 +0900

    함수 이용하여 웹 어플리케이션 refactoring 하기

--- node/ForEach_main.js
+++ node/ForEach_main.js
@@ -2,6 +2,23 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, description){
+    return `
+    <!doctype html>
+<html>
+<head>
+<title>WEB1 - ${title}</title>
+<meta charset="utf-8">
+</head>
+<body>
+<h1><a href="/">WEB</a></h1>
+${list}
+<h2>${title}</h2>
+<p>${description}</p>
+</body>
+</html>
+    `;
+}
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var parsedUrl=url.parse(_url,true);
@@ -23,21 +40,7 @@ var app = http.createServer(function(request,response){
                 i=i+1;
             }
             list = list+'</ul>';
-            var template=`
-                <!doctype html>
-            <html>
-            <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-            </head>
-            <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-            </body>
-            </html>
-                `;
+            var template=templateHTML(title,list,description);
             //200(OK)를 반환
             response.writeHead(200);
             response.end(template);
