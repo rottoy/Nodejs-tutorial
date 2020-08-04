e60b5fc4ea78dcbcc357080275f73914d7871bea
JeonHuiJae <rottoy@konkuk.ac.kr>
2020-08-04 20:04:33 +0900

    콜백 함수 설명 추가

--- node/etc_nodegrammar/callback.js
+++ node/etc_nodegrammar/callback.js
@@ -9,6 +9,7 @@ var result = fs.readFileSync('syntax/sample.txt', 'utf8');
 console.log(result);
 console.log('C');
 */
+// 결과 : A, result, C
 
 //readFileAsync
 console.log('A');
@@ -16,3 +17,5 @@ fs.readFile('../data/sample.txt','utf-8',function(err,result){
     console.log(result);    
 });
 console.log('C');
+
+//결과 : A,C,result(바뀔수도 있음)
