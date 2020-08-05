9d6fa894b0c81eb04d019a1eaaf2a8a851fdedb3
rottoy <wnsgur9609@naver.com>
2020-08-05 13:09:55 +0900

    파일 삭제 구현

--- node/POSTwithForm_main.js
+++ node/POSTwithForm_main.js
@@ -68,7 +68,13 @@ var app = http.createServer(function(request,response){
                     var list =templateList(filelist);
                     var template=templateHTML(title,list,
                         `<h2>${title}</h2>${description}`,
-                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+                        `<a href="/create">create</a>
+                        <a href="/update?id=${title}">update</a>
+                        <form action="delete_process" method='POST'>
+                        <input type='hidden' name='id' value="${title}">
+                        <input type='submit' value='delete'>
+                        </form>
+                       `);
                     //200(OK)를 반환
                     response.writeHead(200);
                     response.end(template);
@@ -150,7 +156,9 @@ var app = http.createServer(function(request,response){
                         <input type="submit">
                     </p>
                     </form>`,
-                    `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+                    `<a href="/create">create</a>
+                    <a href="/update?id=${title}">update</a>
+                    `);
                 //200(OK)를 반환
                 response.writeHead(200);
                 response.end(template);
@@ -160,7 +168,7 @@ var app = http.createServer(function(request,response){
     else if(pathname==='/update_process'){
         var body='';
 
-        //request.on : 데이터가 부분으로 들어옴
+        //request.on : 파일 데이터가 부분으로 들어옴
         request.on('data', function(data){
             body= body+data;
             //body is POST data
@@ -178,6 +186,7 @@ var app = http.createServer(function(request,response){
              console.log(post);
              //File rename
              fs.rename(`data/${id}`,`data/${title}`,function(err){
+                 //File contents rewrite
                 fs.writeFile(`data/${title}`,description,'utf-8',function(err){
                     //Redirection : 302
                     response.writeHead(302,{Location : `/?id=${title}`});
@@ -185,6 +194,29 @@ var app = http.createServer(function(request,response){
                  })
     
 
+             });
+        });
+    }
+    else if(pathname==='/delete_process'){
+        var body='';
+
+        //request.on : 데이터가 부분으로 들어옴
+        request.on('data', function(data){
+            body= body+data;
+            //body is POST data
+        });
+
+        //request.end : 정보 수신이 끝났을 때 실행
+        request.on('end',function(){
+            
+            //return json of body
+             var post = qs.parse(body);
+             var id =post.id;
+
+             //file delete
+             fs.unlink(`data/${id}`,function(error){
+                response.writeHead(302,{Location : `/`});
+                response.end();
              });
         });
     }
